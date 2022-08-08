31f6b319e7d0f262d6ec41b8f8ca0d0b1162b5b7
Jesus Marmol <jesusdavid890@hotmail.com>
2022-08-08 08:39:16 -0500

    Day 39 JS

---
+++ day-39/menu.js
@@ -0,0 +1,13 @@
+var lines = document.querySelector(".lines");
+
+lines.addEventListener("click", () => {
+  lines.classList.add("active");
+  document.querySelector(".menu").classList.add("active");
+});
+
+document.querySelectorAll("li").forEach((a) => {
+  a.addEventListener("click", () => {
+    lines.classList.remove("active");
+    document.querySelector(".menu").classList.remove("active");
+  });
+});
